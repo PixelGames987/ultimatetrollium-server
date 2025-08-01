8a745a0c2ef986c4c305c03d798c2d29a19ec7a4
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2025-08-01 10:48:04 +0000

    Created files and added a setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The setup script for debian server/lxc (Bookworm)
+
+set -e
+
+echo -e "\n[*] Updating package list...\n"
+apt update
+
+echo -e "\n[*] Full system update...\n"
+apt full-upgrade -y
+
+echo -e "\n[*] Installing tools and dependencies...\n"
+apt install hashcat aircrack-ng hcxdumptool -y
+
+echo "[*] Setup completed, scripts ready for use."
